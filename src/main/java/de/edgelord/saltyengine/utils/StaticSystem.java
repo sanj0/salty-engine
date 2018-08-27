a2ec2d0001a6cbd4fd60b38a272556188f9926ee
edgelord <malte.dostal@gmail.com>
2018-08-27 19:03:17 +0200

    Adjust version name within the code

--- src/main/java/de/edgelord/saltyengine/utils/StaticSystem.java
+++ src/main/java/de/edgelord/saltyengine/utils/StaticSystem.java
@@ -18,8 +18,8 @@ import java.awt.event.KeyEvent;
 
 public class StaticSystem {
 
-    public static String versionTag = "0.3-SNAPSHOT";
-    public static double version = 0.3;
+    public static String versionTag = "0.4.1-SNAPSHOT";
+    public static double version = 0.41;
     public static String versionMode = "SNAPSHOT";
     public static String versionName = "Zeus";
     public static String gameName = "My name is Nym. Arno Nym.";
