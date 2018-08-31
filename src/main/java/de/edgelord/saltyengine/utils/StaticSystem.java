f12289154a031802b13c904047482bddb1d8b540
edgelord <malte.dostal@gmail.com>
2018-08-31 11:09:02 +0200

    Flag version as 0.5 due to changes which aren't compatible with older versions

--- src/main/java/de/edgelord/saltyengine/utils/StaticSystem.java
+++ src/main/java/de/edgelord/saltyengine/utils/StaticSystem.java
@@ -20,8 +20,8 @@ import java.awt.event.KeyEvent;
 
 public class StaticSystem {
 
-    public static String versionTag = "0.4.4-SNAPSHOT";
-    public static String version = "0.4.4";
+    public static String versionTag = "0.5-SNAPSHOT";
+    public static String version = "0.5";
     public static VersionMode versionMode = VersionMode.SNAPSHOT;
     public static String versionName = "Zeus";
     public static String gameName = "My name is Nym. Arno Nym.";
@@ -64,7 +64,7 @@ public class StaticSystem {
     public static Output systemOutput = new Output(System.out);
     public static boolean drawFPS = false;
 
-    public static Font font = new Font(Font.SERIF, 0, 15);
+    public static Font font = new Font(Font.SERIF, Font.PLAIN, 15);
 
     public static Scene currentScene = new DummyScene();
     public static LayerCollection currentLayerCollection = new DummyLayerCollection();
