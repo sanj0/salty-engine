712496f7e422bae5a7e52c08d0db5f98031c99ba
edgelord <malte.dostal@gmail.com>
2018-11-19 17:38:04 +0100

    Flag version as 0.14.2-SNAPSHOT

--- src/main/java/de/edgelord/saltyengine/utils/SaltySystem.java
+++ src/main/java/de/edgelord/saltyengine/utils/SaltySystem.java
@@ -38,8 +38,8 @@ import java.awt.*;
 
 public class SaltySystem {
 
-    public static String versionTag = "0.14.1-SNAPSHOT";
-    public static String version = "0.14.1";
+    public static String versionTag = "0.14.2-SNAPSHOT";
+    public static String version = "0.14.2";
     public static VersionMode versionMode = VersionMode.SNAPSHOT;
 
     public static long fixedTickMillis = 1;
